c04ab2584bff64bc2f37a0bd9fde2cdd159a2646
Noah Lively <noahlively@gmail.com>
2026-04-25 12:47:37 -0500

    working on text rendering

--- src/assets/text.rs
+++ src/assets/text.rs
@@ -1,7 +1,50 @@
+use std::{fs, path::Path};
+use serde::Deserialize;
+use std::collections::HashMap;
+
+use anyhow::Context;
+use image::GenericImageView;
+
+use crate::SCREEN_WIDTH;
+
+struct FontGlyph {
+    x: u8,
+    width: u8,
+}
+
+struct FontMetadata {
+    height: u8,
+    altas_width: u8,
+    glyphs: HashMap<u8, FontGlyph>,
+}
+
 struct FontStruct {
-    height: i16,
-    location: [i16; 256]
-    width: [u8; 256],
+    data: image::RgbaImage,
+    metadata: FontMetadata,
+}
+
+impl FontStruct {
+    fn new_from_file(base_path: &std::path::Path, font_name: &str) -> Result<Self, Error> {
+        let metadata_path = base_path.join(format!("{}.json", font_name));
+        let image_path = base_path.join(format!("{}.png", font_name));
+
+        let metadata_json = fs::read_to_string(metadata_path)?;
+
+        let img = image::open(image_path)?;
+        let metadata: FontMetadata = serde_json::from_str(metadata_json.as_str())?;
+
+        // let (width, height) = img.dimensions();
+        let rgba = img.to_rgba8();
+
+        Ok(Self {
+            data: rgba,
+            metadata,
+        })
+    }
+
+    fn character(&self, ascii: u8) -> (u8, u8) {
+        (self.metadata.glyphs[ascii].x, self.metadata.glyphs[ascii].width)
+    }
 }
 
 struct TextDrawContext {
@@ -17,77 +60,41 @@ struct TextDrawResult {
     pub buffer_height: i16,
 }
 
-const LINE_WIDTH: usize = 80; // 320px / 4 planes
-
-
 impl TextDrawContext {
-    pub fn measure_string(&self, s: &str, font: &FontStruct) -> (i16, i16) {
-        let height = font.height;
-        let mut width: i16 = 0;
+    /// renders a string of characters in a single row
+    pub fn draw_string(&mut self, s: &str, fb: &mut [u8], dest_x: u16, dest_y: u16, font: &FontStruct) -> Result<(), Error> {
+        let fb_width = SCREEN_WIDTH;
+        let character_spacing: u8 = 3;
 
-        for b in s.bytes() {
-            // in case we get \0 terminated strings, since this
-            // is a port of an old C program
-            if b == 0 {
-                break;
-            }
-            width += font.width[b as usize] as i16;
-        }
-
-        (width, height)
-    }
-
-    pub fn draw_string(&mut self, s: &str, fb: &[u8], font: &FontStruct) -> TextDrawResult {
-        // this is just a stub until we have the actual font data
-        let font_data: Vec<u8> = Vec::new();
-
-        // int		width,step,height,i;
-        // byte	far *source, far *dest, far *origdest;
-        // byte	far *col_src, far *col_dest;
-        let col_src: &u8;
-        let col_dest: &u8;
-        let mask: u8;
+        let mut fb_offset_x = dest_x;
 
         let height = font.height;
-        let buffer_height = font.height;
 
-        // formerly `dest` in the c code
-        let dest = self.py as usize * LINE_WIDTH + (self.px as usize >> 2);
-        mask = 1 << (self.px & 3);
+        let src_bytes = font.data.as_raw();
 
         for b in s.bytes() {
             // break on \0 string terminator
             if b == 0 {
                 break;
             }
-            let width = font.width[b as usize] as i16;
-            let step = width;
-            let offset = font.location[b as usize] as i16;
-            let source = &font_data[offset];
+            let (offset, width) = font.character(b);
+            
+            for row in 0..font.metadata.height {
+                let sy = row; // source y has no offset because the font image is just 1 row of glyphs
+                let dy = dest_y + row;
 
-            while width > 0 {
-                width -= 1;
-                fb[dest + (self.px & 3) as usize] = self.font_color;
-                col_src  = source;
-                col_dest = dest;
-                for row in 0..height {
-                    if *col_src > 0 {
-                        fb[dest_offset] = self.font_color;
-                    }
-                // 	if (*col_src)
-                // 		*col_dest = fontcolor;
-                // 	col_src  += step;
-                // 	col_dest += linewidth;
-                }
-                // source++;
-                // px++;
-                // mask <<= 1;
-                // if (mask == 16)
-                // {
-                // 	mask = 1;
-                // 	dest++;
-                // }
+                let src_start = ((sy * font.metadata.altas_width + offset) * 4) as usize;
+                let src_end = src_start + (width * 4) as usize;
+
+                let dst_start = ((dy * fb_width + fb_offset_x) * 4) as usize;
+                let dst_end = dst_start + (width * 4) as usize;
+
+                fb[dst_start..dst_end].copy_from_slice(&src_bytes[src_start..src_end]);
             }
+
+            fb_offset_x += width + character_spacing;
         }
+
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,7 @@ pub mod game;
 pub mod input;
 pub mod math;
 pub mod ui;
+
+pub const SCREEN_WIDTH: u32 = 320;
+pub const SCREEN_HEIGHT: u32 = 200;
+pub const SCALE: u32 = 3;
